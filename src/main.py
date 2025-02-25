5ed504542d38582d257697cb210744d94dbf7f78
Loki <getapple@GetApples-MacBook-Air.local>
2025-02-25 15:35:45 +0200

    Обновление стартового экрана

--- src/main.py
+++ src/main.py
@@ -4,50 +4,78 @@ from views.user_info_view import user_info_view
 
 def main(page: ft.Page):
     page.title = "Kivi Retail TEST"
-    page.version = "0.0.1"
+    page.version = "0.0.2"
     page.theme_mode = ft.ThemeMode.SYSTEM  # Системная тема (светлая/темная)
     page.horizontal_alignment = 'center'  # Выравнивание по центру
     page.vertical_alignment = 'center'  # Выравнивание по центру
+    page.adaptive = True
+
+    page.appbar = ft.AppBar(
+        title=ft.Text("KIVI Retail DEV", size=32, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE_600),
+        # actions=[
+        #     ft.IconButton(ft.cupertino_icons.INFO, style=ft.ButtonStyle(padding=0))
+        # ],
+        bgcolor=ft.Colors.with_opacity(0.04, ft.CupertinoColors.SYSTEM_BACKGROUND),
+        # shadow_color=ft.Colors.with_opacity(0.01, ft.CupertinoColors.SYSTEM_BACKGROUND),
+    )
+
+    page.navigation_bar = ft.NavigationBar(
+        destinations=[
+            ft.NavigationBarDestination(
+                icon=ft.Icons.EXPLORE,
+                label="Explore",
+            ),
+            ft.NavigationBarDestination(
+                icon=ft.Icons.COMMUTE,
+                label="Commute",
+            ),
+            ft.NavigationBarDestination(
+                icon=ft.Icons.BOOKMARK_BORDER,
+                selected_icon=ft.Icons.BOOKMARK,
+                label="Bookmark",
+            ),
+        ],
+        bgcolor=ft.Colors.with_opacity(0.04, ft.CupertinoColors.SYSTEM_BACKGROUND),
+    )
+
 
     expanded = False
-    radius_avatar = page.height * 0.05
 
-    def _expand_click(e):
-        nonlocal expanded, radius_avatar
+    def _expand_click(event):
+        nonlocal expanded
         # Toggle expansion state
         expanded = not expanded
         # Update the height and radius based on the state
-        new_height = page.height * 0.8 if expanded else page.height * 0.15
-        radius_avatar = page.height * 0.1 if expanded else page.height * 0.05
-        _top_container.height = new_height
-        _avatar.radius = radius_avatar
-        _title.content.size = 36 if expanded else 32
-        _subtitle.content.size = 28 if expanded else 26
+        new_top_height = page.height * 0.7 if expanded else page.height * 0.15
+        _top_container.height = new_top_height
         _top_container.update()
 
     def _top():
-        global _avatar, _top_container, _title, _subtitle
-        info, _avatar, _title, _subtitle = user_info_view(page, radius_avatar)
+        global _top_container
+        info = user_info_view(page)
         _top_container = ft.Container(
             width=page.width,  # Dynamic width (80% of the screen width)
             height=page.height * 0.15,  # Dynamic height (40% of the screen height)
             gradient=ft.LinearGradient(
                 begin=ft.alignment.bottom_right,
                 end=ft.alignment.top_left,
-                colors=["lightblue600", "lightblue900"],
+                colors=["grey800", "grey900"],
             ),
-            border_radius=35,
+            border_radius=ft.BorderRadius(top_left=15, top_right=15, bottom_left=15, bottom_right=15),
             animate=ft.animation.Animation(duration=350, curve="decelerate"),
             on_click=_expand_click,
             content=ft.Column(
                 alignment="start",
                 controls=[info],
             ),
+            padding=ft.Padding(left=10, top=10, right=10, bottom=10),
+            margin=ft.Margin(left=0, top=0, right=0, bottom=10),
         )
         return _top_container
 
     def _bottom():
-        bottom = ft.Container(
+        global _bottom_container
+        _bottom_container = ft.Container(
             width=page.width,  # Dynamic width (80% of the screen width)
             height=page.height * 0.10,  # Dynamic height (40% of the screen height)
             gradient=ft.LinearGradient(
@@ -56,22 +84,27 @@ def main(page: ft.Page):
                 colors=["lightblue300", "lightblue600"],
             ),
             border_radius=35,
-            # animate=ft.animation.Animation(duration=350, curve="decelerate"),
+            animate=ft.animation.Animation(duration=350, curve="decelerate"),
         )
-        return bottom
+        return _bottom_container
 
-    # Контент страницы
+    def _middle():
+        global _middle_container
+        _middle_container = ft.Container(
+            height=page.height * 0.7,  # Initial height
+            animate=ft.animation.Animation(duration=350, curve="decelerate"),
+        )
+        return _middle_container
+
+    # Page content
     _c = ft.Container(
-        width=page.width,
-        height=page.height,
-        content=ft.Column(
-            width=page.width * 0.95,
-            height=page.height,
+        height=page.height,  # Set the height of the container
+        content=ft.ListView(
+            height=page.height,  # Set the height of the ListView
             controls=[
-                _top(),
-                ft.Container(height=page.height * 0.7),  # Пустое пространство между верхним и нижним контейнерами
-                _bottom()
-            ],
+                _top() for i in range(10)  # Alternate between _top and _bottom
+            ] + [ft.Container(height=100)],  # Add spacing at the end
+            on_scroll=True,
         ),
     )
     
--- src/views/user_info_view.py
+++ src/views/user_info_view.py
@@ -1,22 +1,17 @@
 import flet as ft
 
-def user_info_view(page, radius_avatar):
-    avatar = ft.CircleAvatar(
-        foreground_image_src="https://avatars.githubusercontent.com/u/5041459?s=88&v=4",
-        radius=radius_avatar,
-    )
+def user_info_view(page):
     title = ft.Container(
         content=ft.Text("KIVI Retail DEV", size=32, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE_900),
-        padding=ft.Padding(top=10, right=0, bottom=0, left=0)
+        padding=ft.Padding(top=10, right=10, bottom=10, left=10)
     )
     subtitle = ft.Container(
         content=ft.Text("Олександр Риженков", size=26, text_align=ft.TextAlign.CENTER, color=ft.Colors.GREEN_600),
-        padding=ft.Padding(top=0, right=0, bottom=10, left=0)
+        padding=ft.Padding(top=10, right=10, bottom=10, left=10)
     )
     info = ft.Row(
         alignment="center",
         controls=[
-            avatar,
             ft.Column(
                 [
                     title,
@@ -27,4 +22,4 @@ def user_info_view(page, radius_avatar):
             ),
         ],
     )
-    return info, avatar, title, subtitle
+    return info
