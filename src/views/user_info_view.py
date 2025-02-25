4e2f17ec1e22902d725eb5d23a25bf947722e9bf
Loki <getapple@GetApples-MacBook-Air.local>
2025-02-25 17:38:28 +0200

    Обновление скрола

--- src/views/user_info_view.py
+++ src/views/user_info_view.py
@@ -1,25 +1,39 @@
 import flet as ft
 
 def user_info_view(page):
-    title = ft.Container(
-        content=ft.Text("KIVI Retail DEV", size=32, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE_900),
-        padding=ft.Padding(top=10, right=10, bottom=10, left=10)
-    )
-    subtitle = ft.Container(
-        content=ft.Text("Олександр Риженков", size=26, text_align=ft.TextAlign.CENTER, color=ft.Colors.GREEN_600),
-        padding=ft.Padding(top=10, right=10, bottom=10, left=10)
-    )
-    info = ft.Row(
-        alignment="center",
-        controls=[
-            ft.Column(
-                [
-                    title,
-                    subtitle,
-                ],
-                alignment="spaceBetween",
-                expand=True
+    info = ft.Row([
+        ft.Container(
+            ft.Container(
+                ft.Column([
+                    ft.Text("Информация", size=28, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE_600),
+                    ft.Row([
+                        ft.Icon(ft.Icons.PERSON, size=20, color=ft.Colors.GREEN_600),
+                        ft.Text("ФИО:", size=16, text_align=ft.TextAlign.LEFT, color=ft.Colors.GREEN_600),
+                        ft.Text("Александр Рыженков Александрович", text_align=ft.TextAlign.RIGHT, size=20, color=ft.Colors.BLACK, width=page.width * 0.5),
+                    ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
+                    ft.Row([
+                        ft.Icon(ft.Icons.CALENDAR_TODAY, size=20, color=ft.Colors.PURPLE_600),
+                        ft.Text("Дата рождения:", size=16, color=ft.Colors.PURPLE_600),
+                        ft.Text("31/10/1988", text_align=ft.TextAlign.RIGHT, size=20, color=ft.Colors.BLACK, width=page.width * 0.5),
+                    ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
+                    ft.Row([
+                        ft.Icon(ft.Icons.PHONE, size=20, color=ft.Colors.RED_600),
+                        ft.Text("Телефон:", size=16, color=ft.Colors.RED_600),
+                        ft.Text("+3 999 999 99 99", text_align=ft.TextAlign.RIGHT, size=20, color=ft.Colors.BLACK, width=page.width * 0.5),
+                    ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
+                ]),
+                bgcolor=ft.Colors.GREY_200,
+                border_radius=24,
+                padding=14  
             ),
-        ],
+            bgcolor=ft.Colors.GREY_200,
+            width=page.width * 0.9,
+            border_radius=24,
+            margin=ft.Margin(left=5, right=5, top=5, bottom=5)  
+        )
+    ], alignment=ft.MainAxisAlignment.CENTER
+    
     )
+
+
     return info
