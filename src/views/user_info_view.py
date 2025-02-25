6a0d11184e4c542f6960c952dedcfbbfacafaf06
Loki <getapple@GetApples-MacBook-Air.local>
2025-02-25 17:45:30 +0200

    Адоптация  нижней и верхней панели

--- src/views/user_info_view.py
+++ src/views/user_info_view.py
@@ -4,26 +4,23 @@ def user_info_view(page):
     
     info = ft.Container(
                 ft.Column([
-                    ft.Text("Информация", size=28, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE_600),
+                    ft.Text("Информация", size=28, weight=ft.FontWeight.BOLD, color=ft.Colors.BLUE_200),
                     ft.Row([
                         ft.Icon(ft.Icons.PERSON, size=20, color=ft.Colors.GREEN_600),
                         ft.Text("ФИО:", size=16, text_align=ft.TextAlign.LEFT, color=ft.Colors.GREEN_600),
-                        ft.Text("Александр Рыженков Александрович", text_align=ft.TextAlign.RIGHT, size=20, color=ft.Colors.BLACK, width=page.width * 0.5),
+                        ft.Text("Александр Рыженков Александрович", text_align=ft.TextAlign.RIGHT, size=20, color=ft.Colors.WHITE, width=page.width * 0.5),
                     ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
                     ft.Row([
                         ft.Icon(ft.Icons.CALENDAR_TODAY, size=20, color=ft.Colors.PURPLE_600),
                         ft.Text("Дата рождения:", size=16, color=ft.Colors.PURPLE_600),
-                        ft.Text("31/10/1988", text_align=ft.TextAlign.RIGHT, size=20, color=ft.Colors.BLACK, width=page.width * 0.5),
+                        ft.Text("31/10/1988", text_align=ft.TextAlign.RIGHT, size=20, color=ft.Colors.WHITE, width=page.width * 0.5),
                     ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
                     ft.Row([
                         ft.Icon(ft.Icons.PHONE, size=20, color=ft.Colors.RED_600),
                         ft.Text("Телефон:", size=16, color=ft.Colors.RED_600),
-                        ft.Text("+3 999 999 99 99", text_align=ft.TextAlign.RIGHT, size=20, color=ft.Colors.BLACK, width=page.width * 0.5),
+                        ft.Text("+3 999 999 99 99", text_align=ft.TextAlign.RIGHT, size=20, color=ft.Colors.WHITE, width=page.width * 0.5),
                     ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
                 ]),
-                bgcolor=ft.Colors.GREY_200,
-                border_radius=24,
-                padding=14  
             )
             
 
